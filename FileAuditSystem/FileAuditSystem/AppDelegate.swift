bfe77a32312a112c60dee95bb93410f76365009a
Anoop Vaidya <anoopkumar.vaidya@synchronoss.com>
2023-04-29 06:53:36 +0530

    Create viewcontroller for folder management

--- FileAuditSystem/FileAuditSystem/AppDelegate.swift
+++ FileAuditSystem/FileAuditSystem/AppDelegate.swift
@@ -11,16 +11,28 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
+    
+    let folderManageVC = FolderManageViewController()
 
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    func isExtensionUpAndRunning() -> Bool {
+        return true;
     }
-
+    
+        
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        if isExtensionUpAndRunning() {
+            self.window.contentView?.addSubview(folderManageVC.view)
+        }
+        else {
+            print("check sext")
+        }
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
